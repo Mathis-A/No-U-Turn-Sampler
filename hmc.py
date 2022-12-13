e5e53ecf28073054c4ffdbc163bc42556f5f949b
Mathis Allard <mathis.allard-mevel@centrale.centralelille.fr>
2022-12-13 14:30:18 +0100

    feat: finished simple hmc, added rosenbrock test

--- hmc.py
+++ hmc.py
@@ -4,32 +4,48 @@ from leapfrog import leapfrog
 
 class HamiltonianMonteCarlo:
 
-    def __init__(self, eps, L, loss, grad_loss, num_iterations, seed):
+    def __init__(self, eps, L, logp, grad_logp, num_iterations, seed=None):
         self.rng = np.random.RandomState(seed=seed)
         self.eps = eps
         self.L = L
-        self.loss = loss
-        self.grad_loss = grad_loss
+        self.logp = logp
+        self.grad_logp = grad_logp
         self.num_iterations = num_iterations
 
     def run(self, theta0):
         theta = [theta0]
-        r = [self.rng.multivariate_normal(np.zeros_like(theta0),
-                                          np.eye(len(theta0)))]
-        for m in range(self.M):
-            theta.append(theta[-1].copy())
-            r.append(r[-1].copy())
+        r = []  # Not currently used but could be for visualization.
 
+        for iter_ in range(self.num_iterations):
+            # Set r_0 and theta_m
+            r_0 = self.rng.multivariate_normal(np.zeros_like(theta0),
+                                             np.eye(len(theta0)))
+            r.append(r_0)
+            theta.append(theta[-1].copy())
+
+            # Set theta_tilde and r_tilde
+            r_tilde = r_0.copy()
             theta_tilde = theta[-1].copy()
-            r_tilde = r[-1].copy()
-            for i in range(len(self.L)):
-                theta_tilde = leapfrog(theta_tilde, self.grad_loss, r_tilde,
-                                       self.eps)
-            alpha = min(1, np.exp(self.loss(theta_tilde) -
+
+            # Leapfrog steps to generate a new state
+            for _ in range(self.L):
+                theta_tilde, r_tilde = leapfrog(theta_tilde, r_tilde,
+                                                self.grad_logp,
+                                                self.eps)
+                # Each step could be extracted for visualisation purposes
+
+            # Acceptance probability
+            alpha = min(1, np.exp(self.logp(theta_tilde) -
                                   r_tilde @ r_tilde / 2) /
-                        np.exp(self.loss(theta[-1]) -
-                               r[-1] @ r[-1] / 2))
+                        np.exp(self.logp(theta[-1]) - r_0 @ r_0 / 2))
+
             if self.rng.random() < alpha:
+                # Accept
                 theta[-1] = theta_tilde
                 r[-1] = r_tilde
+            # else: theta[-1] and r[-1] are already set.
         return theta
+
+
+if __name__ == '__main__':
+    pass
--- leapfrog.py
+++ leapfrog.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-def leapfrog(theta, grad_loss, r, eps):
+
+def leapfrog(theta, r, grad_loss, eps):
+    # Single update step
     r_tilde = r + (eps/2)*grad_loss(theta)
     theta_tilde = theta + eps*r_tilde
-    r_tilde+= (eps/2)*grad_loss(theta_tilde)
+    r_tilde += (eps/2)*grad_loss(theta_tilde)
     return theta_tilde, r_tilde
